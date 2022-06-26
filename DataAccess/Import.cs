e6104b769c0f735342060a4951cb06f5d529f672
gorgleb <gorglebworking@gmail.com>
2022-06-26 13:14:06 +0300

    Добавьте файлы проекта.

---
+++ DataAccess/Import.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using DataAccess.Models;
+global using DataAccess.DbAccess;
+global using DataAccess.Models;
+global using Dapper;
+global using System.Data;
+global using Microsoft.Extensions.Configuration;
+global using System.Data.SqlClient;
